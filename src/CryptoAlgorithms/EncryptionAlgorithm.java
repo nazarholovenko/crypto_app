28d512ed92ebb6fc9e1c8b10e37014258051be73
Nazar Holovenko <nazargolovenko336@gmail.com>
2023-07-16 21:39:30 -0700

    added arrays of ukrainian alphabet symbols (upper and lower cases)

--- src/CryptoAlgorithms/EncryptionAlgorithm.java
+++ src/CryptoAlgorithms/EncryptionAlgorithm.java
@@ -1,4 +1,19 @@
 package CryptoAlgorithms;
 
 public class EncryptionAlgorithm {
+    public static final char[] UKRAINIAN_ALPHABET_LOWER_CASE =
+            {'a', 'б', 'в',
+                    'г', 'ґ' , 'д', 'е', 'є', 'ж', 'з',
+                    'и', 'і', 'ї', 'й', 'к', 'л', 'м',
+                    'н', 'о', 'п', 'р', 'с', 'т', 'у',
+                    'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь',
+                    'ю', 'я' };
+
+    public static final char[] UKRAINIAN_ALPHABET_UPPER_CASE =
+            {'А', 'Б', 'В',
+                    'Г', 'Ґ' , 'Д', 'Е', 'Є', 'Ж', 'З',
+                    'И', 'І', 'Ї', 'Й', 'К', 'Л', 'М',
+                    'Н', 'О', 'П', 'Р', 'С', 'Т', 'У',
+                    'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь',
+                    'Ю', 'Я' };
 }
