543b7f190f211c634ee35a3bfc29a167428087f9
Nazar Holovenko <nazargolovenko336@gmail.com>
2023-07-20 19:23:29 -0700

    added private countMatchesWithSpaces method

--- src/CryptoAlgorithms/BruteForceAlgorithm.java
+++ src/CryptoAlgorithms/BruteForceAlgorithm.java
@@ -2,6 +2,21 @@ package CryptoAlgorithms;
 
 
 public class BruteForceAlgorithm {
+    private int countMatchesWithSpaces(String decryptedText, String encryptedText) {
+        int matches = 0;
+        int length = Math.min(decryptedText.length(), encryptedText.length());
 
+        for (int i = 0; i < length; i++) {
+            if (decryptedText.charAt(i) == encryptedText.charAt(i)) {
+                matches++;
+            } else if (decryptedText.charAt(i) == ' ' || encryptedText.charAt(i) == ' ') {
+                matches++;
+            }
+        }
+
+        return matches;
+    }
 }
+
+
 
