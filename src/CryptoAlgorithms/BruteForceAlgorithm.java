cfece1a75dccb0dacc804746688c8d762315bc0c
Nazar Holovenko <nazargolovenko336@gmail.com>
2023-07-20 19:24:50 -0700

    added private decrypt method and imported CryptoKeys.CryptoKey

--- src/CryptoAlgorithms/BruteForceAlgorithm.java
+++ src/CryptoAlgorithms/BruteForceAlgorithm.java
@@ -1,7 +1,13 @@
 package CryptoAlgorithms;
 
 
+import CryptoKeys.CryptoKey;
+
 public class BruteForceAlgorithm {
+    private String decrypt(String encryptedText, CryptoKey key) {
+        DecryptionAlgorithm decryptionAlgorithm = new DecryptionAlgorithm();
+        return decryptionAlgorithm.decrypt(encryptedText, key);
+    }
     private int countMatchesWithSpaces(String decryptedText, String encryptedText) {
         int matches = 0;
         int length = Math.min(decryptedText.length(), encryptedText.length());
