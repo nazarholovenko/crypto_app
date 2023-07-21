6c1f3abc15bc141bf3340ecc863d0ef6b132ae0b
Nazar Holovenko <nazargolovenko336@gmail.com>
2023-07-20 21:05:48 -0700

    added displayMenuText() method

--- src/NavigationBar/MainMenu.java
+++ src/NavigationBar/MainMenu.java
@@ -3,4 +3,12 @@ package NavigationBar;
 import NavigationBar.RunningMode;
 
 public class MainMenu {
+    private void displayMenuText() {
+        System.out.println("===== ГОЛОВНЕ МЕНЮ =====");
+        System.out.println("Виберіть режим:");
+        System.out.println("1. Шифрування");
+        System.out.println("2. Дешифрування");
+        System.out.println("3. Дешифрування методом Brute Force");
+        System.out.println("0. Вихід");
+    }
 }
