5e48b2ef978b3ea4f50baa2460f073d898629682
Nazar Holovenko <nazargolovenko336@gmail.com>
2023-07-20 18:51:26 -0700

    added arrays of ukrainian alphabet symbols (upper and lower cases)

--- src/CryptoAlgorithms/EncryptionAlgorithm.java
+++ src/CryptoAlgorithms/EncryptionAlgorithm.java
@@ -1,5 +1,7 @@
 package CryptoAlgorithms;
 
+import CryptoKeys.CryptoKey;
+
 public class EncryptionAlgorithm {
     public static final char[] UKRAINIAN_ALPHABET_LOWER_CASE =
             {'a', 'б', 'в',
@@ -16,4 +18,40 @@ public class EncryptionAlgorithm {
                     'Н', 'О', 'П', 'Р', 'С', 'Т', 'У',
                     'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь',
                     'Ю', 'Я' };
+
+    public String encrypt(String text, CryptoKey key) {
+        StringBuilder result = new StringBuilder();
+        int moveBy = key.getKey();
+        for (char c : text.toCharArray()) {
+            char encryptedChar = encryptCharacter(c, moveBy);
+            result.append(encryptedChar);
+        }
+        return result.toString();
+    }
+
+    private char encryptCharacter(char character, int moveBy) {
+        char[] alphabet;
+        if (Character.isLowerCase(character)) {
+            alphabet = UKRAINIAN_ALPHABET_LOWER_CASE;
+        } else if (Character.isUpperCase(character)) {
+            alphabet = UKRAINIAN_ALPHABET_UPPER_CASE;
+        } else {
+            return character;
+        }
+        int index = getIndex(character, alphabet);
+        int shiftedIndex = (index + moveBy) % alphabet.length;
+        return alphabet[shiftedIndex];
+    }
+
+    private int getIndex(char character, char[] alphabet) {
+        for (int i = 0; i < alphabet.length; i++) {
+            if (alphabet[i] == character) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
+
+
+
