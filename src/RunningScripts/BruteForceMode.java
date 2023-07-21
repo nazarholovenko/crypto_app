9cacdca2f079f7885a40feb7f6fc222b5aae8402
Nazar Holovenko <nazargolovenko336@gmail.com>
2023-07-20 20:03:12 -0700

    overrided execute() method and created final field for BruteForceMode class

--- src/RunningScripts/BruteForceMode.java
+++ src/RunningScripts/BruteForceMode.java
@@ -1,4 +1,28 @@
 package RunningScripts;
 
-public class BruteForceMode {
+import CryptoAlgorithms.BruteForceAlgorithm;
+import NavigationBar.RunningMode;
+
+import java.util.Scanner;
+
+public class BruteForceMode implements RunningMode {
+    private static final String INPUT_MESSAGE = "Введіть шлях до файлу: ";
+    private static final String CONTENT_CHANGED = "Зміни збережено у файлі: ";
+
+    @Override
+    public void execute() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(INPUT_MESSAGE);
+        String inputFilePath = scanner.nextLine();
+
+        FileReadingLogic fileReadingLogic = new FileReadingLogic();
+        String encryptedText = fileReadingLogic.readFile(inputFilePath);
+
+        BruteForceAlgorithm bruteForceAlgorithm = new BruteForceAlgorithm(encryptedText);
+        bruteForceAlgorithm.bruteForceDecrypt();
+
+
+        fileReadingLogic.writeFile(inputFilePath, bruteForceAlgorithm.getDecryptedText());
+        System.out.println(CONTENT_CHANGED + inputFilePath);
+    }
 }
