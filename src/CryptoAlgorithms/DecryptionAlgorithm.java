a2e10daef960f14c7901b65206c60e745eac2404
Nazar Holovenko <nazargolovenko336@gmail.com>
2023-07-20 19:07:56 -0700

    added getIndex method

--- src/CryptoAlgorithms/DecryptionAlgorithm.java
+++ src/CryptoAlgorithms/DecryptionAlgorithm.java
@@ -3,7 +3,14 @@ package CryptoAlgorithms;
 import CryptoKeys.CryptoKey;
 
 public class DecryptionAlgorithm {
-
+    private int getIndex(char character, char[] alphabet) {
+        for (int i = 0; i < alphabet.length; i++) {
+            if (alphabet[i] == character) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
 
 
