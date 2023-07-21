6535a33e3272bf63847382e96f2af4e76a41bc96
Nazar Holovenko <nazargolovenko336@gmail.com>
2023-07-20 20:18:27 -0700

    added private field and constructor for DecryptionMode class

--- src/RunningScripts/DecryptionMode.java
+++ src/RunningScripts/DecryptionMode.java
@@ -1,5 +1,11 @@
 package RunningScripts;
 
+import CryptoAlgorithms.DecryptionAlgorithm;
+
 public class DecryptionMode {
+    private DecryptionAlgorithm decryptionAlgorithm;
+    public DecryptionMode() {
+        decryptionAlgorithm = new DecryptionAlgorithm();
+    }
 
 }
