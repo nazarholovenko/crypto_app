bc15dde792fc14e59008c0a1b9fbda656c5dbe16
Nazar Holovenko <nazargolovenko336@gmail.com>
2023-07-20 20:58:44 -0700

    overrided execute() method and added final String field with information for user

--- src/RunningScripts/EncryptionMode.java
+++ src/RunningScripts/EncryptionMode.java
@@ -1,3 +1,5 @@
+package RunningScripts;
+
 import CryptoAlgorithms.EncryptionAlgorithm;
 import CryptoKeys.CryptoKey;
 import CryptoKeys.MoveAtOneLetter;
@@ -5,15 +7,35 @@ import CryptoKeys.MoveAtThreeLetters;
 import CryptoKeys.MoveAtTwoLetters;
 import NavigationBar.RunningMode;
 
+
 import java.util.Scanner;
 
 public class EncryptionMode implements RunningMode {
     private static final String ENTER_KEY = "Введіть криптоключ (1, 2 або 3): ";
     private static final String WRONG_KEY = "Неправильний криптоключ. Використовується криптоключ 1.";
+    private static final String ENTER_PATH = "Введіть шлях до файлу для шифрування: ";
+    private static final String ENCRYPTION_SUCCEED = "Текст успішно зашифровано та збережено у файлі ";
     private EncryptionAlgorithm encryptionAlgorithm;
 
     public EncryptionMode() {
         encryptionAlgorithm = new EncryptionAlgorithm();
+    }
+    @Override
+    public void execute() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(ENTER_PATH);
+        String inputFilePath = scanner.nextLine();
+
+        FileReadingLogic fileReadingLogic = new FileReadingLogic();
+        String plainText = fileReadingLogic.readFile(inputFilePath);
+
+        CryptoKey cryptoKey = selectCryptoKey();
+
+        String encryptedText = encryptionAlgorithm.encrypt(plainText, cryptoKey);
+
+
+        fileReadingLogic.writeFile(inputFilePath, encryptedText);
+        System.out.println(ENCRYPTION_SUCCEED + inputFilePath);
     }
     private CryptoKey selectCryptoKey() {
         Scanner scanner = new Scanner(System.in);
