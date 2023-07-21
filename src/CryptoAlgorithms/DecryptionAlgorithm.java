0706d04ed8efcc61de4923b23e5c6307d4693a3c
Nazar Holovenko <nazargolovenko336@gmail.com>
2023-07-20 19:13:43 -0700

    added private decryptCharacter method

--- src/CryptoAlgorithms/DecryptionAlgorithm.java
+++ src/CryptoAlgorithms/DecryptionAlgorithm.java
@@ -3,6 +3,20 @@ package CryptoAlgorithms;
 import CryptoKeys.CryptoKey;
 
 public class DecryptionAlgorithm {
+    private char decryptCharacter(char character, int moveBy) {
+        char[] alphabet;
+        if (Character.isLowerCase(character)) {
+            alphabet = EncryptionAlgorithm.UKRAINIAN_ALPHABET_LOWER_CASE;
+        } else if (Character.isUpperCase(character)) {
+            alphabet = EncryptionAlgorithm.UKRAINIAN_ALPHABET_UPPER_CASE;
+        } else {
+            return character;
+        }
+        int index = getIndex(character, alphabet);
+        int shiftedIndex = (index - moveBy + alphabet.length) % alphabet.length;
+        return alphabet[shiftedIndex];
+    }
+
     private int getIndex(char character, char[] alphabet) {
         for (int i = 0; i < alphabet.length; i++) {
             if (alphabet[i] == character) {
