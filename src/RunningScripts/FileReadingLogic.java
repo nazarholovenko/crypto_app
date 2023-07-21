79b790fdda281f114f22409a1c8bc3426d201dbe
Nazar Holovenko <nazargolovenko336@gmail.com>
2023-07-20 19:47:43 -0700

    added public readFile method

--- src/RunningScripts/FileReadingLogic.java
+++ src/RunningScripts/FileReadingLogic.java
@@ -1,4 +1,20 @@
 package RunningScripts;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FileReadingLogic {
+    public String readFile(String filePath) {
+        StringBuilder content = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append(System.lineSeparator());
+            }
+        } catch (IOException e) {
+            System.out.println("Помилка при зчитуванні файлу: " + e.getMessage());
+        }
+        return content.toString();
+    }
 }
