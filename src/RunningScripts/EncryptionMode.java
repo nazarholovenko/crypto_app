6a8fb4235b2cbb671bd3b4390ad3416d6f3b7a7b
Nazar Holovenko <nazargolovenko336@gmail.com>
2023-07-20 20:51:36 -0700

    added private EncryptionAlgorithm field and created constructor for EncryptionMode class

--- src/RunningScripts/EncryptionMode.java
+++ src/RunningScripts/EncryptionMode.java
@@ -1,5 +1,11 @@
+import CryptoAlgorithms.EncryptionAlgorithm;
 import NavigationBar.RunningMode;
 
 public class EncryptionMode implements RunningMode {
+    private EncryptionAlgorithm encryptionAlgorithm;
+
+    public EncryptionMode() {
+        encryptionAlgorithm = new EncryptionAlgorithm();
+    }
 
 }
